32bf3467e742e2334f94cb6571c62310c2203174
ZhangZachary <zijian96@tongji.edu.cn>
2020-10-14 21:06:30 +0800

    Initial commit

---
+++ SequenceTagger.py
@@ -0,0 +1,50 @@
+from transformers.modeling_bert import *
+from torch.nn.utils.rnn import pad_sequence
+
+
+class BertForSequenceTagging(BertPreTrainedModel):
+	def __init__(self, config):
+		super(BertForSequenceTagging, self).__init__(config)
+		self.num_labels = config.num_labels
+
+		self.bert = BertModel(config)
+		self.dropout = nn.Dropout(config.hidden_dropout_prob)
+		self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+		self.init_weights()
+
+	def forward(self, input_data, token_type_ids=None, attention_mask=None, labels=None,
+				position_ids=None, inputs_embeds=None, head_mask=None):
+		input_ids, input_token_starts = input_data
+		outputs = self.bert(input_ids,
+							attention_mask=attention_mask,
+							token_type_ids=token_type_ids,
+							position_ids=position_ids,
+							head_mask=head_mask,
+							inputs_embeds=inputs_embeds)
+		sequence_output = outputs[0]
+
+		# obtain original token representations from sub_words representations (by selecting the first sub_word)
+		origin_sequence_output = [
+			layer[starts.nonzero().squeeze(1)]
+			for layer, starts in zip(sequence_output, input_token_starts)]
+		padded_sequence_output = pad_sequence(origin_sequence_output, batch_first=True)
+		padded_sequence_output = self.dropout(padded_sequence_output)
+
+		logits = self.classifier(padded_sequence_output)
+
+		outputs = (logits,)
+		if labels is not None:
+			loss_mask = labels.gt(-1)
+			loss_fct = CrossEntropyLoss()
+			# Only keep active parts of the loss
+			if loss_mask is not None:
+				active_loss = loss_mask.view(-1) == 1
+				active_logits = logits.view(-1, self.num_labels)[active_loss]
+				active_labels = labels.view(-1)[active_loss]
+				loss = loss_fct(active_logits, active_labels)
+			else:
+				loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+			outputs = (loss,) + outputs
+
+		return outputs
---
+++ metrics.py
@@ -0,0 +1,153 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from collections import defaultdict
+
+import numpy as np
+
+
+def get_entities(seq, suffix=False):
+    # for nested list
+    if any(isinstance(s, list) for s in seq):
+        seq = [item for sublist in seq for item in sublist + ['O']]
+    prev_tag = 'O'
+    prev_type = ''
+    begin_offset = 0
+    chunks = []
+    for i, chunk in enumerate(seq + ['O']):
+        if suffix:
+            tag = chunk[-1]
+            type_ = chunk.split('-')[0]
+        else:
+            tag = chunk[0]
+            type_ = chunk.split('-')[-1]
+
+        if end_of_chunk(prev_tag, tag, prev_type, type_):
+            chunks.append((prev_type, begin_offset, i-1))
+        if start_of_chunk(prev_tag, tag, prev_type, type_):
+            begin_offset = i
+        prev_tag = tag
+        prev_type = type_
+
+    return chunks
+
+
+def end_of_chunk(prev_tag, tag, prev_type, type_):
+    chunk_end = False
+
+    if prev_tag == 'E': chunk_end = True
+    if prev_tag == 'S': chunk_end = True
+
+    if prev_tag == 'B' and tag == 'B': chunk_end = True
+    if prev_tag == 'B' and tag == 'S': chunk_end = True
+    if prev_tag == 'B' and tag == 'O': chunk_end = True
+    if prev_tag == 'I' and tag == 'B': chunk_end = True
+    if prev_tag == 'I' and tag == 'S': chunk_end = True
+    if prev_tag == 'I' and tag == 'O': chunk_end = True
+
+    if prev_tag != 'O' and prev_tag != '.' and prev_type != type_:
+        chunk_end = True
+
+    return chunk_end
+
+
+def start_of_chunk(prev_tag, tag, prev_type, type_):
+    chunk_start = False
+
+    if tag == 'B': chunk_start = True
+    if tag == 'S': chunk_start = True
+
+    if prev_tag == 'E' and tag == 'E': chunk_start = True
+    if prev_tag == 'E' and tag == 'I': chunk_start = True
+    if prev_tag == 'S' and tag == 'E': chunk_start = True
+    if prev_tag == 'S' and tag == 'I': chunk_start = True
+    if prev_tag == 'O' and tag == 'E': chunk_start = True
+    if prev_tag == 'O' and tag == 'I': chunk_start = True
+
+    if tag != 'O' and tag != '.' and prev_type != type_:
+        chunk_start = True
+
+    return chunk_start
+
+
+def f1_score(y_true, y_pred, average='micro', digits=2, suffix=False):
+    true_entities = set(get_entities(y_true, suffix))
+    pred_entities = set(get_entities(y_pred, suffix))
+
+    nb_correct = len(true_entities & pred_entities)
+    nb_pred = len(pred_entities)
+    nb_true = len(true_entities)
+
+    p = 100 * nb_correct / nb_pred if nb_pred > 0 else 0
+    r = 100 * nb_correct / nb_true if nb_true > 0 else 0
+    score = 2 * p * r / (p + r) if p + r > 0 else 0
+
+    return score
+
+
+def accuracy_score(y_true, y_pred):
+    if any(isinstance(s, list) for s in y_true):
+        y_true = [item for sublist in y_true for item in sublist]
+        y_pred = [item for sublist in y_pred for item in sublist]
+
+    nb_correct = sum(y_t==y_p for y_t, y_p in zip(y_true, y_pred))
+    nb_true = len(y_true)
+
+    score = nb_correct / nb_true
+
+    return score
+
+
+def classification_report(y_true, y_pred, digits=2, suffix=False):
+    true_entities = set(get_entities(y_true, suffix))
+    pred_entities = set(get_entities(y_pred, suffix))
+
+    name_width = 0
+    d1 = defaultdict(set)
+    d2 = defaultdict(set)
+    for e in true_entities:
+        d1[e[0]].add((e[1], e[2]))
+        name_width = max(name_width, len(e[0]))
+    for e in pred_entities:
+        d2[e[0]].add((e[1], e[2]))
+
+    last_line_heading = 'avg / total'
+    width = max(name_width, len(last_line_heading), digits)
+
+    headers = ["precision", "recall", "f1-score", "support"]
+    head_fmt = u'{:>{width}s} ' + u' {:>9}' * len(headers)
+    report = head_fmt.format(u'', *headers, width=width)
+    report += u'\n\n'
+
+    row_fmt = u'{:>{width}s} ' + u' {:>9.{digits}f}' * 3 + u' {:>9}\n'
+
+    ps, rs, f1s, s = [], [], [], []
+    for type_name, true_entities in d1.items():
+        pred_entities = d2[type_name]
+        nb_correct = len(true_entities & pred_entities)
+        nb_pred = len(pred_entities)
+        nb_true = len(true_entities)
+
+        p = 100 * nb_correct / nb_pred if nb_pred > 0 else 0
+        r = 100 * nb_correct / nb_true if nb_true > 0 else 0
+        f1 = 2 * p * r / (p + r) if p + r > 0 else 0
+
+        report += row_fmt.format(*[type_name, p, r, f1, nb_true], width=width, digits=digits)
+
+        ps.append(p)
+        rs.append(r)
+        f1s.append(f1)
+        s.append(nb_true)
+
+    report += u'\n'
+
+    # compute averages
+    report += row_fmt.format(last_line_heading,
+                             np.average(ps, weights=s),
+                             np.average(rs, weights=s),
+                             np.average(f1s, weights=s),
+                             np.sum(s),
+                             width=width, digits=digits)
+
+    return report
