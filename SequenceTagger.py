82bb50e3d641948b7c78bffb809714cb5ebfd6ac
96-Zachary <zijian96@tongji.edu.cn>
2020-10-27 22:30:58 +0800

    Update SequenceTagger.py

--- SequenceTagger.py
+++ SequenceTagger.py
@@ -1,3 +1,5 @@
+from torchcrf  import CRF
+import torch.nn.functional as F
 from transformers.modeling_bert import *
 from torch.nn.utils.rnn import pad_sequence
 
@@ -48,3 +50,27 @@ class BertForSequenceTagging(BertPreTrainedModel):
 			outputs = (loss,) + outputs
 
 		return outputs
+
+log_soft = F.log_softmax
+class Bert_CRF(BertPreTrainedModel):
+	def __init__(self, config):
+		super(Bert_CRF, self).__init__(config)
+		self.num_labels = config.num_labels
+		self.bert = BertModel(config)
+		self.dropout = nn.Dropout(config.hidden_dropout_prob)
+		self.classifier = nn.Linear(config.hidden_size, self.num_labels)
+		self.init_weights()
+		self.crf = CRF(self.num_labels, batch_first=True)
+
+	def forward(self, input_ids, attn_masks, labels=None):  # dont confuse this with _forward_alg above.
+		outputs = self.bert(input_ids, attn_masks)
+		sequence_output = outputs[0]
+		sequence_output = self.dropout(sequence_output)
+		emission = self.classifier(sequence_output)
+		attn_masks = attn_masks.type(torch.uint8)
+		if labels is not None:
+			loss = -self.crf(log_soft(emission, 2), labels, mask=attn_masks, reduction='mean')
+			return loss
+		else:
+			prediction = self.crf.decode(emission, mask=attn_masks)
+			return prediction
